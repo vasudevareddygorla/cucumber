97298b2fa2960af5713d2bfad2908280f5a4d6b5
Vasu G <vasudeva@clipbook.io>
2024-04-01 17:10:15 +0530

    weekly test runner

---
+++ src/test/java/runners/WeeklyTestRunner.java
@@ -0,0 +1,17 @@
+package runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = {"stepdefinitions", "utils"},
+        tags = "@weekly",
+        plugin = { "pretty", "html:target/cucumber", "json:target/weekly-tests.json" },
+        monochrome = true
+)
+public class WeeklyTestRunner {
+}
