0ceaff1ffac41064748f413f710b748922baa85f
Vasu G <vasudeva@clipbook.io>
2024-04-01 13:30:04 +0530

    modified options

--- src/test/java/utils/Hooks.java
+++ src/test/java/utils/Hooks.java
@@ -25,8 +25,14 @@ public class Hooks {
             WebDriverManager.chromedriver().setup();
             ChromeOptions options = new ChromeOptions();
             options.addArguments("--headless");
+            options.addArguments("start-maximized"); // open Browser in maximized mode
+            options.addArguments("disable-infobars"); // disabling infobars
+            options.addArguments("--disable-extensions"); // disabling extensions
+            options.addArguments("--disable-gpu"); // applicable to windows os only
+            options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+            options.addArguments("--no-sandbox"); // Bypass OS security model
             driver = new ChromeDriver(options);
-            driver.manage().window().maximize();
+//            driver.manage().window().maximize();
             driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
             wait = new WebDriverWait(driver,2);
     }
